91da18e4adb67105438db45c8452c4ad0e5f0702
Zonc-hub <sonesone222006@gmail.com>
2025-07-23 16:23:29 +0000

    feat: Add interactive form and HTML templates

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
 from flask import Flask
 
-# 'app' ဆိုတဲ့ variable ကို gunicorn က ရှာပြီး run မှာဖြစ်ပါတယ်
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    # ဒီစာသားကို သင်၏ website မှာ မြင်ရပါလိမ့်မယ်
-    return "Hello Zonc! This is the first successful deployment."
+def hello_world():
+    return 'Hello from Flask!'
 
-# ဒီအပိုင်းက သင်၏ local computer မှာ တိုက်ရိုက် run စမ်းကြည့်ဖို့အတွက်ပါ
-# Render က ဒီအပိုင်းကို အသုံးမပြုပါဘူး
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
